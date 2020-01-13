3a58bba89d9d864c48823ad7abe02941bd158bc8
Zsolti <zsolt.borcsok@gmail.com>
2020-01-13 12:30:54 +0100

    Support collection properties by the RuntimeBean interface

--- src/main/java/org/nting/data/bean/RuntimeBean.java
+++ src/main/java/org/nting/data/bean/RuntimeBean.java
@@ -1,8 +1,13 @@
 package org.nting.data.bean;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.nting.data.Property;
+import org.nting.data.property.ListProperty;
+import org.nting.data.property.MapProperty;
+import org.nting.data.property.SetProperty;
 
 public interface RuntimeBean {
 
@@ -12,6 +17,18 @@ public interface RuntimeBean {
 
     default <T> Property<T> getProperty(Object propertyId) {
         return getProperty(propertyId.toString());
+    }
+
+    default <E> ListProperty<E> getListProperty(Object propertyId) {
+        return (ListProperty<E>) this.<List<E>> getProperty(propertyId.toString());
+    }
+
+    default <E> SetProperty<E> getSetProperty(Object propertyId) {
+        return (SetProperty<E>) this.<Set<E>> getProperty(propertyId.toString());
+    }
+
+    default <K, V> MapProperty<K, V> getMapProperty(Object propertyId) {
+        return (MapProperty<K, V>) this.<Map<K, V>> getProperty(propertyId.toString());
     }
 
     default <T> T getValue(Object propertyId) {
