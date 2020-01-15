05f7862fe4b338de3b64f0bff270588240702dd5
Zsolti <zsolt.borcsok@gmail.com>
2020-01-15 20:11:05 +0100

    Generify StringToListConverter and provide a test for it.

--- src/main/java/org/nting/data/converter/StringToListConverter.java
+++ src/main/java/org/nting/data/converter/StringToListConverter.java
@@ -1,29 +1,36 @@
 package org.nting.data.converter;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import com.google.common.base.Converter;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
 
-public class StringToListConverter extends Converter<String, List<String>> {
+public class StringToListConverter<T> extends Converter<String, List<T>> {
+
+    public static StringToListConverter<String> stringToStringListConverter(String separator) {
+        return new StringToListConverter<>(separator, Converter.identity());
+    }
 
     private final Joiner joiner;
     private final Splitter splitter;
+    private final Converter<String, T> converter;
 
-    public StringToListConverter(String separator) {
+    public StringToListConverter(String separator, Converter<String, T> converter) {
         joiner = Joiner.on(separator);
         splitter = Splitter.on(separator);
+        this.converter = converter;
     }
 
     @Override
-    protected List<String> doForward(String s) {
-        return Lists.newLinkedList(splitter.split(s));
+    protected List<T> doForward(String s) {
+        return StreamSupport.stream(splitter.split(s).spliterator(), false).map(converter).collect(Collectors.toList());
     }
 
     @Override
-    protected String doBackward(List<String> strings) {
-        return joiner.join(strings);
+    protected String doBackward(List<T> list) {
+        return joiner.join(list.stream().map(converter.reverse()).collect(Collectors.toList()));
     }
 }
---
+++ src/test/java/org/nting/data/converter/StringToListConverterTest.java
@@ -0,0 +1,47 @@
+package org.nting.data.converter;
+
+import static org.junit.Assert.assertEquals;
+import static org.nting.data.converter.StringConverters.enumConverter;
+import static org.nting.data.model.Priority.BLOCKER;
+import static org.nting.data.model.Priority.CRITICAL;
+import static org.nting.data.model.Priority.MAJOR;
+import static org.nting.data.model.Priority.MINOR;
+import static org.nting.data.model.Priority.TRIVIAL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.nting.data.Property;
+import org.nting.data.model.Priority;
+import org.nting.data.property.ListProperty;
+
+import com.google.common.collect.ImmutableList;
+
+public class StringToListConverterTest {
+
+    private ListProperty<Priority> prioritiesProperty;
+    private Property<String> stringProperty;
+
+    @Before
+    public void setUp() {
+        prioritiesProperty = new ListProperty<>(MAJOR, BLOCKER, CRITICAL);
+        stringProperty = prioritiesProperty
+                .convert(new StringToListConverter<>(", ", enumConverter(Priority.class)).reverse());
+    }
+
+    @Test
+    public void testGetValue() {
+        assertEquals("MAJOR, BLOCKER, CRITICAL", stringProperty.getValue());
+
+        prioritiesProperty.add(MINOR);
+        assertEquals("MAJOR, BLOCKER, CRITICAL, MINOR", stringProperty.getValue());
+
+        prioritiesProperty.remove(0);
+        assertEquals("BLOCKER, CRITICAL, MINOR", stringProperty.getValue());
+    }
+
+    @Test
+    public void testSetValue() {
+        stringProperty.setValue("MINOR, TRIVIAL");
+        assertEquals(ImmutableList.of(MINOR, TRIVIAL), prioritiesProperty.getValue());
+    }
+}
