d322794918fec81100c3d515456b78749a481d84
Zsolti <zsolt.borcsok@gmail.com>
2020-01-20 20:00:08 +0100

    Support priority configuration without subclassing each time

--- src/main/java/org/nting/data/ValueChangeListener.java
+++ src/main/java/org/nting/data/ValueChangeListener.java
@@ -8,4 +8,19 @@ public interface ValueChangeListener<T> {
     default int priority() {
         return 1000;
     }
+
+    default ValueChangeListener<T> withPriority(int priority) {
+        return new ValueChangeListener<T>() {
+
+            @Override
+            public void valueChange(ValueChangeEvent<T> event) {
+                ValueChangeListener.this.valueChange(event);
+            }
+
+            @Override
+            public int priority() {
+                return priority;
+            }
+        };
+    }
 }
